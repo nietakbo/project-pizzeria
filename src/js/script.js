ee66ba6257d80e5fab4881de16cb540110aba828
Julia Nowak <julianowak077@gmail.com>
2023-11-23 19:41:11 +0100

    add class and accordion

---
+++ src/js/script.js
@@ -0,0 +1,124 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+  'use strict';
+
+const select = {
+  templateOf: {
+    menuProduct: "#template-menu-product",
+    },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
+    },
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
+    },
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
+
+  const classNames = {
+    menuProduct: {
+      wrapperActive: 'active',
+      imageVisible: 'active',
+    },
+  };
+
+  const settings = {
+    amountWidget: {
+      defaultValue: 1,
+      defaultMin: 0,
+      defaultMax: 10,
+    }
+  };
+
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  };
+
+  class Product{
+    constructor(id,data){
+      const thisProduct=this;
+      thisProduct.id=id;
+      thisProduct.data=data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+      console.log('new product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct=this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+      const thisProduct=this;
+
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      clickableTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+          if(activeProduct !== null && activeProduct !== thisProduct.element){
+            activeProduct.classList.remove('active');
+          }
+        thisProduct.element.classList.toggle('active');
+      });
+
+    }
+  }
+
+  const app = {
+    initMenu: function(){
+      const thisApp=this;
+
+      console.log('thisApp.data', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+
+      // const testProduct = new Product();
+      // console.log('testProduct:', testProduct);
+
+    },
+
+    initData: function(){
+      const thisApp=this;
+
+      thisApp.data=dataSource;
+    },
+
+    init: function(){
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
+    },
+  };
+
+  app.init();
+}
