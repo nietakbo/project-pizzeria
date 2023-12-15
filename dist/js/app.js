7d2bcf7e89ffd0d399db6d03a6ddac189c7593f0
Julia Nowak <julianowak077@gmail.com>
2023-12-15 19:17:36 +0100

    update file segregation

--- dist/js/app.js
+++ dist/js/app.js
@@ -42,6 +42,9 @@ import Cart from "./components/Cart.js";
       thisApp.cart = new Cart(cartElem);
 
       thisApp.productList = document.querySelector(select.containerOf.menu);
+      thisApp.productList.addEventListener('add-to-cart', function(event){
+        app.cart.add(event.detail.product);
+      });
     },
 
     init: function(){
